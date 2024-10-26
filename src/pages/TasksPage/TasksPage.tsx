fa0cffa28b97e52c932f8e651b7e011d69a2ac6a
D3NCH11K <daniilotreshko@mail.ru>
2024-10-26 16:03:43 +0300

    breadcrumbs added
    Добавил полузаглушечные крошки

--- src/pages/TasksPage/TasksPage.tsx
+++ src/pages/TasksPage/TasksPage.tsx
@@ -5,9 +5,16 @@ import BreadCrumbs from '../../components/BreadCrumbs/BreadCrumbs';
 import KanbanBoard from '../../components/KanbanBoard/KanbanBoard';
 
 const TaskPage: React.FC = () => {
+
+  const breadcrumbs = [
+    { name: "Проекты", path: "/projects" },
+    { name: "Проект_1", path: "/" }, // заглушка
+    { name: "Задачи", path: "/tasks" },
+  ]; // в целом всё заглушка
+
   return (
     <div className="task-page">
-      <div className="title">Задачи</div>
+      <BreadCrumbs breadcrumbs={breadcrumbs} />
       <div className="task-titles">
         <TaskTitle type="backlog"/>
         <TaskTitle type="inProgress" />
