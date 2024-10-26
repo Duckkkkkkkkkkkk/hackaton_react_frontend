e63912e3d2398b5dc934b46e67c76a747b7ed22c
D3NCH11K <daniilotreshko@mail.ru>
2024-10-27 00:25:14 +0300

    modal burger
    добавл модалку в бургер на мобилках

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,22 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 import "./Header.css";
-import icon_geryon from "../../images/icons/icon_geryon.svg";
-import icon_webpraktik from "../../images/icons/icon_webpraktik.svg"
-import icon_burger from "../../images/icons/icon_burger.svg"
-import { useLayout } from '../Context/LayoutContext';
+import icon_webpraktik from "../../images/icons/icon_webpraktik.svg";
+import icon_burger from "../../images/icons/icon_burger.svg";
+import icon_projects from "../../images/icons/icon_projects.svg";
+import icon_profile from "../../images/icons/icon_profile.svg";
+import icon_exit from "../../images/icons/icon_exit.svg";
+import { useLayout } from "../Context/LayoutContext";
 
 const Header: React.FC = () => {
   const { showHeader } = useLayout();
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const location = useLocation();
 
   if (!showHeader) {
     return null;
   }
 
+  const handleBurgerClick = () => {
+    setIsModalOpen(!isModalOpen);
+  };
+
+  const isActive = (path: string) => location.pathname === path;
+
   return (
-    <header className="header">
-      {<img className="icon_webpraktik" src={icon_webpraktik} alt="webpractice" />}
-      {<img className="icon_burger" src={icon_burger} alt="burger" />}
-    </header>
+    <>
+      <header className="header">
+        <img className="icon_webpraktik" src={icon_webpraktik} alt="webpractice" />
+        <img className="icon_burger" src={icon_burger} alt="burger" onClick={handleBurgerClick} />
+      </header>
+
+      {/* Модальное окно */}
+      <div className={`modal-overlay ${isModalOpen ? "show" : ""}`} onClick={handleBurgerClick}>
+        <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+          <ul>
+            <li>
+              <Link
+                to="/projects"
+                onClick={handleBurgerClick}
+                className={isActive("/projects") ? "active" : ""}
+              >
+                <img src={icon_projects} alt="Проекты" className="icon" />
+                <span>Проекты</span>
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/profile"
+                onClick={handleBurgerClick}
+                className={isActive("/profile") ? "active" : ""}
+              >
+                <img src={icon_profile} alt="Личный кабинет" className="icon" />
+                <span>Личный кабинет</span>
+              </Link>
+            </li>
+            <li>
+              <Link
+                to="/"
+                onClick={() => {
+                  handleBurgerClick();
+                  localStorage.removeItem("access_token");
+                  localStorage.removeItem("user_role");
+                }}
+                className={isActive("/") ? "active" : ""}
+              >
+                <img src={icon_exit} alt="Выйти" className="icon" />
+                <span>Выйти</span>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
   );
 };
 
